433c285ac7b8b41ab08ca41ba4cf9dafeb73315b
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-29 19:21:22 -0800

    Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
 
 function App() {
-  return(
-      <>
-      <Review />
-      </>
-  )
+  return (
+    <>
+      <main>
+          <section className="container">
+              <div className="title">
+                  <h2>Reviews</h2>
+                  <div className="underline"></div>
+              </div>
+              <Review />
+          </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
