311a0c1a396c76649a65c85a0bf5db182a4342e4
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-29 19:24:18 -0800

    Index state + testing people

--- src/Review.js
+++ src/Review.js
@@ -3,6 +3,9 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+    const [index, setIndex] = useState(0);
+    console.log(people);
+    
   return <h2>Review component</h2>;
 };
 
