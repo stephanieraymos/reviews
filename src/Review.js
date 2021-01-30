db4fce80ac26223b3de5c2074d202bfba6b0c224
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-29 19:45:47 -0800

    Image display

--- src/Review.js
+++ src/Review.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
     const [index, setIndex] = useState(0);
-    const {name, job, image, text } = people[index];
-    
-  return <h2>Review component</h2>;
+    const { name, job, image, text } = people[index];
+
+    return <article className="review">
+        <div className="img-container">
+            <img src={image} alt={name} className="person-img"/>
+        </div>
+    </article>;
 };
 
 export default Review;
