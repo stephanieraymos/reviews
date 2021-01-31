90b814d3a9d51996feffd8ec076637d881f40afb
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-30 18:50:43 -0800

    onClicks + nextPerson function

--- src/Review.js
+++ src/Review.js
@@ -3,23 +3,38 @@ import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-    const [index, setIndex] = useState(0);
-    const { name, job, image, text } = people[index];
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
 
-    return <article className="review">
-        <div className="img-container">
-            <img src={image} alt={name} className="person-img"/>
-            <span className="quote-icon"><FaQuoteRight/></span>
-        </div>
-        <h4 className="author">{name}</h4>
-        <p className="job">{job}</p>
-        <p className="info">{text}</p>
-        <div className="button-container">
-            <button className="prev-btn"><FaChevronLeft /></button>
-            <button className="next-btn"><FaChevronRight /></button>
-        </div>
-            <button className="random-btn">Surprise Me!</button>
-    </article>;
+
+  const nextPerson = () => {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        return newIndex;
+      })
+  }
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">Surprise Me!</button>
+    </article>
+  );
 };
 
 export default Review;
